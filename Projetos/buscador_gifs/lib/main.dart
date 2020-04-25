1283b8efbdc05c614c28f360538987f145357ce2
feliperibeirosp <felipe.ribeirosp@gmail.com>
2020-04-24 21:08:35 -0300

    Subindo Projetos

---
+++ Projetos/buscador_gifs/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:buscadorgifs/ui/home_page.dat.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white)
+  ));
+}
